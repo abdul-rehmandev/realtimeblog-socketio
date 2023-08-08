a9202dae9a27c8c63d3e5ce34680d839868a8d42
Abdul Rehman <abdulrehmandev001@gmail.com>
2023-08-08 13:06:36 +0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,127 @@
+// server.js
+
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://127.0.0.1:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    },
+});
+
+const port = 4000; // Replace with your desired port number
+
+// Replace 'mongodb://localhost:27017' with your MongoDB connection string
+const mongoURI = 'mongodb://localhost:27017/realtime-practise';
+
+mongoose.connect(mongoURI).then((res) => {
+    console.log("DB Connected :", res.connection.host)
+}).catch((err) => {
+    console.log(err)
+});
+
+const blogPostSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    timestamp: { type: Date, default: Date.now },
+});
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+io.on('connection', (socket) => {
+    console.log('client connected', socket.id);
+
+    const changeStream = BlogPost.watch();
+
+    changeStream.on('change', (change) => {
+        // Send the change data to the connected client
+        socket.emit('postChange', change);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A client disconnected');
+    });
+});
+
+app.use(cors({
+    origin: "http://127.0.0.1:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}))
+app.use(express.json());
+
+// Define an API endpoint to handle adding a new blog post
+app.post('/api/posts', async (req, res) => {
+    try {
+        const { title, content } = req.body;
+
+        // Create a new blog post using the Mongoose model
+        const newPost = new BlogPost({
+            title,
+            content,
+        });
+
+        // Save the new post to the database
+        await newPost.save();
+
+        // Emit the new post to connected clients through socket.io
+        io.emit('postChange', {
+            operationType: 'insert',
+            fullDocument: newPost.toObject(),
+        });
+
+        // Respond with the newly created post
+        res.status(201).json(newPost);
+    } catch (error) {
+        console.error('Error creating blog post:', error);
+        res.status(500).json({ error: 'Failed to create blog post' });
+    }
+});
+
+app.get('/api/posts', async (req, res) => {
+    try {
+        const posts = await BlogPost.find().sort({ timestamp: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error('Error fetching blog posts:', error);
+        res.status(500).json({ error: 'Failed to fetch blog posts' });
+    }
+});
+
+app.put('/api/posts/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const { title, content } = req.body;
+
+        const updatedPost = await BlogPost.findByIdAndUpdate(
+            postId,
+            { title, content },
+            { new: true }
+        );
+
+        if (!updatedPost) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        io.emit('postChange', {
+            operationType: 'update',
+            documentKey: { _id: postId },
+            fullDocument: updatedPost.toObject(),
+        });
+
+        res.json(updatedPost);
+    } catch (error) {
+        console.error('Error updating blog post:', error);
+        res.status(500).json({ error: 'Failed to update blog post' });
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,128 @@
+// src/App.js
+
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState({ title: '', content: '' });
+
+  useEffect(() => {
+    const socket = io('http://localhost:4000'); // Replace with your server URL
+
+    // Function to update the state with new blog posts
+    const updatePosts = (change) => {
+      if (change.operationType === 'insert') {
+        setPosts((prevPosts) => [...prevPosts, change.fullDocument]);
+      } else if (change.operationType === 'update') {
+        setPosts((prevPosts) =>
+          prevPosts.map((post) =>
+            post._id === change.documentKey._id ? change.fullDocument : post
+          )
+        );
+      } else if (change.operationType === 'delete') {
+        setPosts((prevPosts) =>
+          prevPosts.filter((post) => post._id !== change.documentKey._id)
+        );
+      }
+    };
+
+    socket.on('postChange', updatePosts);
+
+    return () => {
+      socket.disconnect(); // Clean up the socket connection when the component unmounts
+    };
+  }, []);
+
+  useEffect(() => {
+    // Fetch initial blog posts from the backend using Mongoose
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/api/posts'); // Replace with your backend API endpoint
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error('Error fetching blog posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewPost((prevPost) => ({ ...prevPost, [name]: value }));
+  };
+
+  const handleAddPost = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/api/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPost),
+      });
+
+      const data = await response.json();
+      console.log('New blog post created:', data);
+
+      // Clear the input fields after successful post creation
+      setNewPost({ title: '', content: '' });
+    } catch (error) {
+      console.error('Error creating blog post:', error);
+    }
+  };
+
+  const handleUpdatePost = async (postId, updatedData) => {
+    try {
+      const response = await fetch(`http://localhost:4000/api/posts/${postId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData),
+      });
+
+      const updatedPost = await response.json();
+      console.log('Updated blog post:', updatedPost);
+    } catch (error) {
+      console.error('Error updating blog post:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Real-time Blog Posts</h1>
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          <button onClick={() => handleUpdatePost(post._id, { title: 'Updated Title', content: 'Updated Content' })}>
+            Update Post
+          </button>
+          <hr />
+        </div>
+      ))}
+      <div>
+        <h2>Add New Post</h2>
+        <input
+          type="text"
+          name="title"
+          value={newPost.title}
+          onChange={handleInputChange}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          value={newPost.content}
+          onChange={handleInputChange}
+          placeholder="Content"
+        />
+        <button onClick={handleAddPost}>Add Post</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
